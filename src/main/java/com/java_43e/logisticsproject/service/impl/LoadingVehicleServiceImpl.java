25b14acc32a43c2689007fbb8818cbd585f779a0
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-07-24 20:30:24 +0200

    Volume+3 commit

--- src/main/java/com/java_43e/logisticsproject/service/impl/LoadingVehicleServiceImpl.java
+++ src/main/java/com/java_43e/logisticsproject/service/impl/LoadingVehicleServiceImpl.java
@@ -15,29 +15,27 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class LoadingVehicleServiceImpl implements LoadingVehicleService {
 
-
     private final CargoRepository cargoRepository;
     private final OrderRepository orderRepository;
     private final VehicleRepository vehicleRepository;
 
-
     @Override
     @Transactional
     public void loadVehicle(Integer orderId) {
-        // 1. Найти заказ по orderId
+        // 1. Поиск заказа по orderId
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new ResourceNotFoundException("Order with id: " + orderId + " not found"));
 
-        // 2. Если заказ закрыт, выбросить исключение или выполнить другие действия
+        // 2. Если заказ закрыт, выбрасывается исключение
         if (order.isClosed()) {
             throw new IllegalStateException("Order is already closed.");
         }
 
-        // 3. Найти груз по orderId из таблицы "cargos"
+        // 3. Поиск груза по orderId из таблицы "cargos"
         Cargo cargo = cargoRepository.findById(order.getOrderId())
                 .orElseThrow(() -> new ResourceNotFoundException("Cargo with id: " + order.getOrderId() + " not found"));
 
-        // 4. Присвоить "boolean isAccepted" в таблице "cargos"
+        // 4. Присвоение "boolean isAccepted" в таблице "cargos"
         cargo.setAccepted(true);
         cargoRepository.save(cargo);
 
@@ -51,8 +49,28 @@ public class LoadingVehicleServiceImpl implements LoadingVehicleService {
             throw new IllegalStateException("Vehicle's maximum volume exceeded.");
         }
 
-        // 6. Обновить "Integer volumeCurrent" в таблице "vehicles"
+        // 6. Обновяем "Integer volumeCurrent" в таблице "vehicles"
         vehicle.setVolumeCurrent(newVolumeCurrent);
+
+
+        int newWeightCurrent = vehicle.getWeightCurrent() + cargo.getWeight();
+        if (newWeightCurrent > vehicle.getWeightMax()) {
+            // Выбрасываем исключение и отменяем транзакцию
+            throw new IllegalStateException("Vehicle's maximum weight exceeded.");
+        }
+        // 7. Обновить "Integer weightCurrent" в таблице "vehicles"
+        vehicle.setWeightCurrent(newWeightCurrent);
+
+
+        int newPlacePalletCurrent = vehicle.getPlacePalletCurrent() + cargo.getPlacePallet();
+        if (newPlacePalletCurrent > vehicle.getPlacePalletMax()) {
+            // Выбрасываем исключение и отменяем транзакцию
+            throw new IllegalStateException("Vehicle's maximum placePallet exceeded.");
+        }
+        // 8. Обновить "Integer placePalletCurrent" в таблице "vehicles"
+        vehicle.setPlacePalletCurrent(newPlacePalletCurrent);
+
+        // Сохраняем изменения в таблице "vehicles"
         vehicleRepository.save(vehicle);
     }
 }
